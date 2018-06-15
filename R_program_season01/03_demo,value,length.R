b4a85483851e1e6d629f210a8055c699636a7ca1
Yunah Baek <yunah0515@gmail.com>
2018-06-16 02:04:13 +0900

    feat: Add 03_demo,value,length.R

---
+++ R_program_season01/03_demo,value,length.R
@@ -0,0 +1,27 @@
+# demo
+demo(persp)
+demo(graphics)
+
+# value
+x <- 1
+y <- 2
+x*y -> z
+z     # 2
+
+x <- c(1,2,3)
+x
+y <- c(2,3,4)
+y
+x*y   # 2,6,12
+
+z <- 9
+x*z   # 9,18,27
+
+a <- c(1,2)
+x*a   # error
+
+# length()
+length(x) # 3
+length(y) # 3
+length(z) # 1
+length(a) # 2
