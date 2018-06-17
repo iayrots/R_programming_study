204217a1bf2893a39096f9570bc36d90ccc402e0
Yunah Baek <yunah0515@gmail.com>
2018-06-17 22:53:19 +0900

    feat: Add 04_data type,true,false.R

---
+++ R_program_season01/04_data type,true,false.R
@@ -0,0 +1,25 @@
+x <- c(1,2,3,4,6)
+y <- c('1','2','3','4','5')
+z <- c('how','are','you','?')
+length(x)  # 5
+length(y)  # 5
+x*y        # error
+
+x
+# string or character
+y
+z
+
+# str() : 테이블의 구조나 데이터의 type을 알 수 있음
+str(x)    # num
+str(y)    # chr
+str(z)    # chr
+
+# boolean
+1+2==3    # TRUE
+1+5==7    # FALSE
+
+x
+y
+x==y      # T T T T F
+
